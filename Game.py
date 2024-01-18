92ec710573c1fc81aedef6962a4ea5bb281a868e
Panagiotis Kapsaliaris <panoskaps002@gmail.com>
2024-01-18 18:15:48 +0200

    lol

--- Game.py
+++ Game.py
@@ -3,7 +3,7 @@ from sys import exit
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600)) #Set Resolution
+screen = pygame.display.set_mode((800, 600)) #Set Resolution    h,w
 pygame.display.set_caption("Another Tempest Clone") #Sets Name For The Game
 clock = pygame.time.Clock() #Object To Control The Framerate
 
@@ -15,10 +15,11 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
     # draw all our elements
     # update everything
     
-    screen.blit(surface,(100, 0))
+    screen.blit(surface,(200, 100)) # Left = Left/Right | Right = Up/Down
             
     pygame.display.update()
     clock.tick(60)  #60FPS/Second
