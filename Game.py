7394853a3edb4ac38849d65979f3d6495770edc4
Panagiotis Kapsaliaris <panoskaps002@gmail.com>
2024-01-18 17:33:16 +0200

    Added main game file

---
+++ Game.py
@@ -0,0 +1,25 @@
+import pygame
+from sys import exit
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600)) #Set Resolution
+pygame.display.set_caption("Tempest") #Sets Name For The Game
+clock = pygame.time.Clock() #Object To Control The Framerate
+
+surface = pygame.Surface((100, 200))
+surface.fill('Red')
+
+while True:
+    for event in pygame.event.get(): #Checks for Events From Keyboard Or Mouse
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+            
+    # draw all our elements
+    # update everything
+    
+    screen.blit(surface,(100, 0))
+            
+    pygame.display.update()
+    clock.tick(60)  #60FPS/Second
