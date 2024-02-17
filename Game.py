7194cc90414a121a2e3d5296ccc7903e9252904b
Dimosthenis Krallis <dimosthenis.krallis@outlook.com>
2024-02-17 02:00:43 +0200

    Finished movement and now going for rotation

--- Game.py
+++ Game.py
@@ -10,38 +10,61 @@ dpi = 96
 resolution = (800, 600)
 
 
+def importPolygonFromSvg(svgfile):
+    levelTree = ET.parse(svgfile)
+    levelRoot = levelTree.getroot()
+    print()
+    path = "./g/path"
+    path = re.sub("/", "/{http://www.w3.org/2000/svg}", path)
+    levelGeom = levelRoot.find(path).attrib["d"]
+    # levelGeom.pop(len(levelGeom) - 1)
+    levelGeom = re.sub("[zlLZ]", "", levelGeom)
+    levelGeom = " ".join(levelGeom.split())
+    print(levelGeom)
+    levelGeom = levelGeom.split(" ")
+    symbol = levelGeom.pop(0)
+    print(levelGeom)
+    tmpLevelGeom = []
+    for i in levelGeom:
+        tmpLevelGeom.append([float(i.split(",")[0]), float(i.split(",")[1])])
+    levelGeom = milimetersToPixels(np.array(tmpLevelGeom.copy()))
+    if symbol == "m":
+        levelGeom = np.cumsum(levelGeom, axis=0)
+    return levelGeom
+
+
 class Player:
-    def __init__(self, c):
+    def __init__(self, c, svgfile):
         self.position = 0  # The game is effectively cyclical 2D as far as the position.
         self.cycle = c  # It is cyclical but some levels arent so we will set negative values for noncyclical levels
-        self.pointList = np.array([[2, 0, 1], [2, 0, 0], [-2, 0, -1]])
-        self.movablePoint = 0
+        self.pointList = importPolygonFromSvg(svgfile)
+        self.depth = 0
 
     def moveLeft(self):
         nocyc = 0
-        cycle = self.cycle
+        tmpcycle = self.cycle
         if self.cycle < 0:
             nocyc = 1
-            cycle = cycle * (-1)
-        position = self.position + 1
-        if position > cycle:
-            position = position - 1
-            if nocyc == 0:
-                position = 0
-        self.position = position
+            tmpcycle = tmpcycle * (-1)
+        tmpposition = self.position + 1
+        if tmpposition > tmpcycle - 1:
+            tmpposition = tmpposition - 1
+            if nocyc == 1:
+                tmpposition = 0
+        self.position = tmpposition
 
     def moveRight(self):
         nocyc = 0
-        cycle = self.cycle
+        tmpcycle = self.cycle
         if self.cycle < 0:
             nocyc = 1
-            cycle = cycle * (-1)
-        position = self.position - 1
-        if position < 0:
-            position = position + 1
-            if nocyc == 0:
-                position = cycle
-        self.position = position
+            tmpcycle = tmpcycle * (-1)
+        tmpposition = self.position - 1
+        if tmpposition < 0:
+            tmpposition = tmpposition + 1
+            if nocyc == 1:
+                tmpposition = tmpcycle - 1
+        self.position = tmpposition
 
     def __str__(self):
         iscyc = ""
@@ -57,27 +80,13 @@ class Level:
     def __init__(self, lvlnum, enLst, svgfile):
         self.levelNumber = lvlnum
         self.enemyList = enLst
-        self.polygonPoints = self.importPolygonFromSvg(svgfile)
+        self.polygonPoints = importPolygonFromSvg(svgfile)
         self.cyclical = self.importCyclicalFromSvg(svgfile)
         self.positions = self.getPositionsFromPolygon(self.polygonPoints)
+        self.positionAngles = self.getPosAnglesFromPolygon(self.polygonPoints)
 
     def getPolygonPoints(self):
         return self.polygonPoints
-
-    def importPolygonFromSvg(self, svgfile):
-        levelTree = ET.parse(svgfile)
-        levelRoot = levelTree.getroot()
-        print()
-        path = "./g/path"
-        path = re.sub("/", "/{http://www.w3.org/2000/svg}", path)
-        levelGeom = levelRoot.find(path).attrib["d"].split(" ")
-        levelGeom.pop(0)
-        levelGeom.pop(len(levelGeom) - 1)
-        tmpLevelGeom = []
-        for i in levelGeom:
-            tmpLevelGeom.append([float(i.split(",")[0]), float(i.split(",")[1])])
-        levelGeom = milimetersToPixels(np.array(tmpLevelGeom.copy()))
-        return levelGeom
 
     def importCyclicalFromSvg(self, svgfile):
         levelTree = ET.parse(svgfile)
@@ -85,23 +94,17 @@ class Level:
         path = "./g/text"
         path = re.sub("/", "/{http://www.w3.org/2000/svg}", path)
         levelCyc = levelRoot.find(path)
-        if levelCyc == "None":
+        if levelCyc is None:
             return True
         return False
 
-    def generateScaledPolygonVerts(self, scale):
-        tmpScaled = []
-        for i in self.polygonPoints:
-            tmpScaled.append(np.array(i) - np.array(resolution)/2)
-
-        scaled = self.polygonPoints*scale
-        return scaled
-
     def getPositionsFromPolygon(self, polygonPoints):
         positions = []
+        print("test")
         for i in range(len(polygonPoints) - 1):
             positions.append((polygonPoints[i] + polygonPoints[i + 1]) / 2.0)
-        if bool(self.cyclical):
+        if self.cyclical is True:
+            print("is cyclical")
             positions.append(
                 (polygonPoints[0] + polygonPoints[len(polygonPoints) - 1]) / 2.0
             )
@@ -109,18 +112,47 @@ class Level:
 
     def getCyclicalForPlayer(self):
         if not self.cyclical:
-            return (-1) * len(self.getPolygonPoints())
-        return len(self.getPolygonPoints())
+            return len(self.positions)
+        return (-1) * len(self.positions)
 
-    def localToLevelSpace(self, positionInLevel, pointArray):
-        print(self.positions[positionInLevel])
-        print(pointArray)
+    def localToLevelSpace(self, positionInLevel, pArray):
+        # print(self.positions[positionInLevel])
+        # print(pointArray)
+        objectCenter = np.sum(pArray, axis=0)/pArray.shape[0]
+        pointArray = np.matmul(
+            [np.cos(self.positionAngles[positionInLevel]), -np.sin(self.positionAngles[positionInLevel]),]
+
+            )
         endPosition = []
         for i in pointArray:
-            print(i)
-            endPosition.append(np.array(i) + self.positions[positionInLevel])
+            # print(i)
+            endPosition.append(np.array(i) + self.positions[positionInLevel] - objectCenter)
         endPosition = np.array(endPosition)
         return endPosition
+
+    def getPosAnglesFromPolygon(self, polygonPoints):
+        angles = []
+        print("test")
+        for i in range(len(polygonPoints) - 1):
+            tmpangle = polygonPoints[i] - polygonPoints[i+1]
+            if tmpangle[1] == 0:
+                tmpangle = 0
+            elif tmpangle[0] == 0:
+                tmpangle = np.pi/2.0
+            else:
+                tmpangle = tmpangle[1]/tmpangle[0]
+            angles.append(tmpangle)
+        if self.cyclical is True:
+            print("is cyclical")
+            tmpangle = polygonPoints[0] - polygonPoints[len(polygonPoints) - 1]
+            if tmpangle[1] == 0:
+                tmpangle = 0
+            elif tmpangle[0] == 0:
+                tmpangle = np.pi/2.0
+            else:
+                tmpangle = tmpangle[1]/tmpangle[0]
+            angles.append(tmpangle)
+        return angles
 
     def __str__(self):
         iscyc = ""
@@ -129,14 +161,31 @@ class Level:
         return f"Level with number {self.levelNumber} and {self.positions} positions that is {iscyc}cyclical. Points making the shape: {self.polygonPoints}, Enemy list: {self.enemyList}"
 
 
+def scaleAgainstCenter(scale, polygonPoints, center):
+    objectCenter = np.sum(polygonPoints, axis = 0)/polygonPoints.shape[0]
+    # print(f"center:  {objectCenter}")
+    scaled = (polygonPoints - objectCenter)*scale + objectCenter
+    scaled += center
+    return scaled
+
+
 def milimetersToPixels(pointArray):
     return pointArray*dpi*0.03937008
 
 
+def drawLinesForLevel(polygonPoints, scaledPolygonPoints, screen, color, width):
+    for i in range(polygonPoints.shape[0]):
+        pygame.draw.line(screen, color, polygonPoints[i], scaledPolygonPoints[i], width)
+
+
 def DrawGame(player, level, screen):
     # pygame.draw.polygon(screen, ())
+    scaled = scaleAgainstCenter(0.1, level.polygonPoints, np.array([0, 0]))
     pygame.draw.polygon(screen, (255, 255, 255), level.getPolygonPoints(), 1)
-    pygame.draw.polygon(screen, (0, 255, 0), level.localToLevelSpace(player.position, player.pointList))
+    pygame.draw.polygon(screen, (255, 255, 255), scaled, 1)
+    # print(level.localToLevelSpace(player.position, player.pointList))
+    pygame.draw.polygon(screen, (0, 255, 0), level.localToLevelSpace(player.position, player.pointList), 1)
+    drawLinesForLevel(level.getPolygonPoints(), scaled, screen, (255, 255, 255), 1)
 
 
 def DrawMainMenu():
@@ -153,7 +202,7 @@ LevelList = []
 LevelList.append(Level(1, [], "testsvg.svg"))
 
 BaseLevel = LevelList[0]
-BasePlayer = Player(BaseLevel.getCyclicalForPlayer())
+BasePlayer = Player(BaseLevel.getCyclicalForPlayer(), "Player.svg")
 
 print(BaseLevel)
 print(BasePlayer)
@@ -171,10 +220,18 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        if event.type == pygame.KEYDOWN:
+            match event.key:
+                case pygame.K_LEFT:
+                    BasePlayer.moveLeft()
+                case pygame.K_RIGHT:
+                    BasePlayer.moveRight()
+            print(BasePlayer)
+    screen.fill("black")
     DrawGame(BasePlayer, BaseLevel, screen)
     DrawPauseMenu()
     DrawMainMenu()
     # print("screen")
-    pygame.display.update()
-    # pygame.display.flip()
+    # pygame.display.update()
+    pygame.display.flip()
     clock.tick(60)  # 60 Frames/Second
